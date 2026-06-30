0b39d17be466da58555771ad230394fca3bccfea
Banderan <bartoszwojcik2004@gmail.com>
2026-06-30 14:27:34 +0200

    fix: smooth removal of projects

--- src/app/features/projects/list/projects-list.component.ts
+++ src/app/features/projects/list/projects-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ProjectService } from '../../../core/services/project.service';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
@@ -15,7 +15,11 @@ export class ProjectsListComponent {
   private readonly projectService = inject(ProjectService);
   private readonly destroyRef = inject(DestroyRef);
 
-  readonly projects = toSignal(this.projectService.loadProjects(), { initialValue: [] });
+  readonly projects = this.projectService.projects;
+
+  constructor() {
+    this.projectService.loadProjects().pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+  }
 
   readonly showNewForm = signal(false);
   readonly newName = signal('');
